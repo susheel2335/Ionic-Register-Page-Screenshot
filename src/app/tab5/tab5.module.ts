6f1ed872cac4b0987b1f0f6dc7c4552d769b2466
Your Name <you@example.com>
2020-11-05 12:42:50 -0900

    First Version

---
+++ src/app/tab5/tab5.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonicModule } from '@ionic/angular';
+
+import { Tab5PageRoutingModule } from './tab5-routing.module';
+
+import { Tab5Page } from './tab5.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ExploreContainerComponentModule,
+    Tab5PageRoutingModule
+  ],
+  declarations: [Tab5Page]
+})
+export class Tab5PageModule {}
